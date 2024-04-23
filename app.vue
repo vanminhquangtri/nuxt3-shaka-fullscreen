1cbb5c81d4f47deb5a5947aef78a3c03f1c3af5d
minhnv60 <minhnv60@fpt.com.vn>
2024-04-23 13:46:09 +0700

    demo shaka

--- app.vue
+++ app.vue
@@ -1,5 +1,85 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- The data-shaka-player-container tag will make the UI library place the controls in this div.
+         The data-shaka-player-cast-receiver-id tag allows you to provide a Cast Application ID that
+           the cast button will cast to; the value provided here is the sample cast receiver. -->
+    <div
+      data-shaka-player-container
+      style="max-width: 40em"
+      data-shaka-player-cast-receiver-id="07AEE832"
+    >
+      <!-- The data-shaka-player tag will make the UI library use this video element.
+            If no video is provided, the UI will automatically make one inside the container div. -->
+      <video
+        autoplay
+        muted
+        playsinline
+        data-shaka-player
+        id="video"
+        style="width: 100%; height: 100%"
+      ></video>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+// @ts-nocheck
+const manifestUri =
+  'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
+
+async function init() {
+  // When using the UI, the player is made automatically by the UI object.
+  const video = document.getElementById('video');
+
+  const ui = video['ui'];
+  const controls = ui.getControls();
+  const player = controls.getPlayer();
+
+  // Attach player and ui to the window to make it easy to access in the JS console.
+  window.player = player;
+  window.ui = ui;
+
+  // Listen for error events.
+  player.addEventListener('error', onPlayerErrorEvent);
+  controls.addEventListener('error', onUIErrorEvent);
+
+  // Try to load a manifest.
+  // This is an asynchronous process.
+  try {
+    await player.load(manifestUri);
+    // This runs if the asynchronous load is successful.
+    console.log('The video has now been loaded!');
+  } catch (error) {
+    onPlayerError(error);
+  }
+}
+
+function onPlayerErrorEvent(errorEvent) {
+  // Extract the shaka.util.Error object from the event.
+  onPlayerError(errorEvent.detail);
+}
+
+function onPlayerError(error) {
+  // Handle player error
+  console.error('Error code', error.code, 'object', error);
+}
+
+function onUIErrorEvent(errorEvent) {
+  // Extract the shaka.util.Error object from the event.
+  onPlayerError(errorEvent.detail);
+}
+
+function initFailed(errorEvent) {
+  // Handle the failure to load; errorEvent.detail.reasonCode has a
+  // shaka.ui.FailReasonCode describing why.
+  console.error('Unable to load the UI library!');
+}
+
+// Listen to the custom shaka-ui-loaded event, to wait until the UI is loaded.
+onBeforeMount(() => {
+  document.addEventListener('shaka-ui-loaded', init);
+  // Listen to the custom shaka-ui-load-failed event, in case Shaka Player fails
+  // to load (e.g. due to lack of browser support).
+  document.addEventListener('shaka-ui-load-failed', initFailed);
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/shaka-player/4.7.13/shaka-player.ui.min.js',
+        },
+      ],
+      link: [
+        {
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/shaka-player/4.7.13/controls.min.css',
+          rel: 'stylesheet',
+        },
+      ],
+    },
+  },
+});
